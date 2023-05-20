24a3574efa532f2d3f2c8e61afe0c18cfe874bd4
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-05-20 16:47:18 +0200

    Chore: Init and configuration dependencies project.

---
+++ PlaningPoker.Api.Test/GameFeature.cs
@@ -0,0 +1,16 @@
+namespace PlaningPoker.Api.Test
+{
+    public class GameFeature
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

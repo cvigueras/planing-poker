b317569b605d598afd3f3dd93ff333d023c6a2a4
cvigueras <carlosj.vigueras@gmail.com>
2023-05-27 12:34:04 +0200

    Refactor: extracted GameUpdateFeature to generic PlaningPokerClient methods.

--- PlaningPoker.Api.Test/GameUpdateFeature.cs
+++ PlaningPoker.Api.Test/GameUpdateFeature.cs
@@ -1,41 +1,35 @@
 ﻿using Newtonsoft.Json;
 using PlaningPoker.Api.Test.Startup;
-using System.Text;
 using webapi;
 
 namespace PlaningPoker.Api.Test
 {
     public class GameUpdateFeature
     {
-        private HttpClient client;
+        private PlaningPokerClient client;
         private const string MediaType = "application/json";
 
         [SetUp]
         public void Setup()
         {
-            client = new SetupFixture().CreateClient();
+            client = new PlaningPokerClient(new SetupFixture().CreateClient());
         }
 
         [Test]
         public async Task RetrieveAGameWithNewUserAdded()
         {
-            using var jsonReaderPost = new StreamReader("./Fixtures/Game.json");
-            var jsonPost = await jsonReaderPost.ReadToEndAsync();
+            var json = await client.GetJsonContent("./Fixtures/Game.json");
 
-            var responsePost = await client!.PostAsync("Game", new StringContent(jsonPost,
-                Encoding.Default,
-                MediaType));
-            responsePost.EnsureSuccessStatusCode();
+            var responsePost = await client.Post("Game", json);
             var gameId = responsePost.Content.ReadAsStringAsync().Result;
 
             var userAddDto = new UsersAddDto("Pedro", gameId);
             var serializedDto = JsonConvert.SerializeObject(userAddDto, Formatting.Indented);
-            var responsePut = await client!.PutAsync($"Game/{gameId}", new StringContent(serializedDto, Encoding.Default,
-                MediaType));
-            responsePut.EnsureSuccessStatusCode();
+            var responsePut = await client.Put($"Game/{gameId}", serializedDto);
 
             var content = await responsePut.Content.ReadAsStringAsync();
             var result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(content), Formatting.Indented);
+
             var settings = new VerifySettings();
             settings.ScrubInlineGuids();
 
---
+++ PlaningPoker.Api.Test/PlaningPokerClient.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace PlaningPoker.Api.Test
+{
+    public class PlaningPokerClient
+    {
+        private const string MediaType = "application/json";
+        private readonly HttpClient client;
+
+        public PlaningPokerClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<string> GetJsonContent(string path)
+        {
+            using var jsonReaderPost = new StreamReader(path);
+            return await jsonReaderPost.ReadToEndAsync();
+        }
+
+        public async Task<HttpResponseMessage> Post(string requestUri, string jsonPost)
+        {
+            var responsePost = await client!.PostAsync(requestUri, new StringContent(jsonPost,
+                Encoding.Default,
+                MediaType));
+            responsePost.EnsureSuccessStatusCode();
+            return responsePost;
+        }
+
+        public async Task<HttpResponseMessage> Put(string requestUri, string json)
+        {
+            var responsePut = await client!.PutAsync(requestUri, new StringContent(json, Encoding.Default,
+                MediaType));
+            responsePut.EnsureSuccessStatusCode();
+            return responsePut;
+        }
+    }
+}

f2a06abc1a5be2b36ec66e32fcd26057edc67a04
cvigueras <carlosj.vigueras@gmail.com>
2023-06-04 17:07:38 +0200

    Feat: Printed in screen cards with differents colors.

--- PlaningPoker.App/src/components/GamePlaning.vue
+++ PlaningPoker.App/src/components/GamePlaning.vue
@@ -4,7 +4,7 @@
             Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationvue">https://aka.ms/jspsintegrationvue</a> for more details.
         </div>
 
-        <div v-if="post" class="content">
+        <div v-if="cards" class="content">
             <label for="urlShare">Share the game</label>
             <input type="text" id="urlShare" name="urlValue" :value="urlValue">
             <button @click="copyUrl">Copy url</button>
@@ -26,9 +26,9 @@
                 </tr>
             </table>
             <div class="cards">
-
-            </div>
-            <div class="infoGame">
+                <div v-for="card in cards" v-bind:key="card.value" :class="getCardClass(card)">
+                    {{ card.value }}
+                </div>
             </div>
         </div>
     </div>
@@ -58,7 +58,7 @@
         data() {
             return {
                 loading: false,
-                post: null
+                cards: this.cards
             };
         },
         created() {
@@ -75,12 +75,12 @@
         },
         methods: {
             fetchData() {
-                this.post = null;
+                this.cards = null;
                 this.loading = true;
 
                 axios.get('cards')
                     .then(response => {
-                        this.post = response.data;
+                        this.cards = response.data;
                         this.loading = false;
                         return;
                     }).catch(error => console.log(error))
@@ -90,12 +90,116 @@
                 copyText.select();
                 copyText.setSelectionRange(0, 99999);
                 navigator.clipboard.writeText(copyText.value);
+            },
+            getCardClass(card) {
+                switch (card.value) {
+                    case "?":
+                        return "card interrogant";
+                    case "coffee":
+                        return "card coffee";
+                    case "0":
+                        return "card cero";
+                    case "0,5":
+                        return "card cerocinco"
+                    case "1":
+                        return "card uno";
+                    case "2":
+                        return "card dos";
+                    case "3":
+                        return "card tres";
+                    case "5":
+                        return "card cinco";
+                    case "8":
+                        return "card ocho";
+                    case "13":
+                        return "card trece";
+                    case "20":
+                        return "card veinte";
+                    case "40":
+                        return "card cuarenta";
+                    case "100":
+                        return "card cien";
+                    default:
+                        return "card interrogant";
+                }
             }
         },
-    };
+    }
 </script>
 
 <style scoped>
+
+    .interrogant {
+        background-color: #FF5733;
+    }
+
+    .coffee {
+        background-color: #CEFF33;
+    }
+
+    .cero {
+        background-color: #A233FF;
+    }
+
+    .cerocinco {
+        background-color: #FF337D;
+    }
+
+    .uno {
+        background-color: #D4FF33;
+    }
+
+    .dos {
+        background-color: #E0FF33;
+    }
+
+    .tres {
+        background-color: #33FFB5;
+    }
+
+    .cinco {
+        background-color: #FFB233;
+    }
+
+    .ocho {
+        background-color: #33E3FF;
+    }
+
+    .trece {
+        background-color: #3361FF;
+    }
+
+    .veinte {
+        background-color: #FC33FF;
+    }
+
+    .cuarenta {
+        background-color: #4FFF33;
+    }
+
+    .cien {
+        background-color: #FFFF33;
+    }
+
+    .cards {
+        position: relative;
+        float: left;
+        width: 100%;
+    }
+
+    .card {
+        position: relative;
+        float: left;
+        border: 1px solid black;
+        width: 65px;
+        height: 100px;
+        margin: 2% 0 0 4%;
+        line-height: 100px;
+        font-weight: bold;
+        font-size: 21px;
+        color: white;
+        border-style: dotted;
+    }
 
     .infoGame {
         position: relative;

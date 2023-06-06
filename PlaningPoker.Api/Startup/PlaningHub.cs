dce6d034e69093abaece00af899a6d8e55c63a83
cvigueras <carlosj.vigueras@gmail.com>
2023-06-06 16:37:19 +0200

    Refactor: reordered methods in PLaningHub.

--- PlaningPoker.Api/Startup/PlaningHub.cs
+++ PlaningPoker.Api/Startup/PlaningHub.cs
@@ -4,6 +4,30 @@ namespace PlaningPoker.Api.Startup;
 
 public class PlaningHub : Hub
 {
+    public async Task JoinGroup(string group, string user)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        await PublishUser(group, user);
+    }
+
+    public async Task PublishUser(string group, string user)
+    {
+        await Clients.Group(group).SendAsync("OnJoinGroup", user);
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception ex)
+    {
+        await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+        await base.OnDisconnectedAsync(ex);
+    }
+
+    //Unused functionality
     public async Task SendMessageToAll(string user, string message)
     {
         await Clients.All.SendAsync("OnReceiveMessage", user, message);
@@ -19,31 +43,8 @@ public class PlaningHub : Hub
         return Clients.Client(connectionId).SendAsync("OnReceiveMessage", message);
     }
 
-    public async Task JoinGroup(string group, string user)
-    {
-        await Groups.AddToGroupAsync(Context.ConnectionId, group);
-        await PublishUser(group, user);
-    }
-    
-    public async Task PublishUser(string group, string user)
-    {
-        await Clients.Group(group).SendAsync("OnJoinGroup", user);
-    }
-
     public async Task SendMessageToGroup(string group, string message)
     {
         await Clients.Group(group).SendAsync("OnReceiveMessage", message);
-    }
-
-    public override async Task OnConnectedAsync()
-    {
-        await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
-        await base.OnConnectedAsync();
-    }
-
-    public override async Task OnDisconnectedAsync(Exception ex)
-    {
-        await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
-        await base.OnDisconnectedAsync(ex);
     }
 }

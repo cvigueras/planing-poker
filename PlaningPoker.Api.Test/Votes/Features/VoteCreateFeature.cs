db6a6e617b9b6f2de3f745bc92a828acbf487979
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-07-19 16:40:35 +0200

    test: RetrieveAGameAfterPostAsync

---
+++ PlaningPoker.Api.Test/Votes/Features/VoteCreateFeature.cs
@@ -0,0 +1,28 @@
+﻿using PlaningPoker.Api.Test.PlaningHub.Fixtures;
+using PlaningPoker.Api.Test.Startup;
+
+namespace PlaningPoker.Api.Test.Votes.Features
+{
+    public class CreateVoteFeature
+    {
+        public const string RequestUriBase = "Vote";
+        public const string PathJson = "./Votes/Fixtures/Vote.json";
+        private PlaningPokerClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new PlaningPokerClient(new SetupFixture().CreateClient());
+        }
+
+        [Test]
+        public async Task RetrieveAGameAfterPostAsync()
+        {
+            var json = await client.GetJsonContent(PathJson);
+
+            var response = await client.Post(RequestUriBase, json);
+            
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}

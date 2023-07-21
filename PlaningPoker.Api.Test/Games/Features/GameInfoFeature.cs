a6b87134504e4d2c84b2e52ceeb1e81bf48c9e94
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-07-21 17:35:32 +0200

    test: RetrieveNumberTotalMatchsAfterGetAsync

---
+++ PlaningPoker.Api.Test/Games/Features/GameInfoFeature.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using PlaningPoker.Api.Test.PlaningHub.Fixtures;
+using PlaningPoker.Api.Test.Startup;
+
+namespace PlaningPoker.Api.Test.Games.Features
+{
+    public class GameInfoFeature
+    {
+        public const string RequestUriBase = "Game/GetNumberMatchs";
+        private PlaningPokerClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new PlaningPokerClient(new SetupFixture().CreateClient());
+        }
+
+        [Test]
+        public async Task RetrieveNumberTotalMatchsAfterGetAsync()
+        {
+            var responsePost = await client.Get(RequestUriBase);
+            var game = responsePost.Content.ReadAsStringAsync().Result;
+            var gameResult = JsonConvert.DeserializeObject<long>(game);
+
+            gameResult.Should().BeGreaterThanOrEqualTo(0);
+        }
+    }
+}

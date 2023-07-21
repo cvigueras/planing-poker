4504b31cede623270ebe91139ea83c5cb97a9a8c
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-07-21 17:15:18 +0200

    feat: included info panels in home.

--- PlaningPoker.App/src/components/HomePlaning.vue
+++ PlaningPoker.App/src/components/HomePlaning.vue
@@ -16,6 +16,17 @@
             Join game
         </button>
     </div>
+
+    <div class="divInfoMatch">
+        <div>200 partidas jugadas hasta hoy.</div>
+        <div class="iconInfoMatch">&#9731;</div>
+    </div>   
+    
+    <div class="divInfoMatch">
+        <div>400 jugadores han pasado por aquí</div>
+        <div class="iconInfoUser">&#9879;</div>
+    </div>
+    
 </template>
 
 <script>

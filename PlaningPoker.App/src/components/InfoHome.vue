6c7e7b5dd6caa3d84940cb3ca57decd6bba2b1fd
Carlos Vigueras <carlosj.vigueras@gmail.com>
2023-07-21 23:29:47 +0200

    feat: implemented numbers of matchs in home.

--- PlaningPoker.App/src/components/InfoHome.vue
+++ PlaningPoker.App/src/components/InfoHome.vue
@@ -1,18 +1,46 @@
 <template>
-    <div class="divInfoMatch">
-        <div>200 partidas jugadas hasta hoy.</div>
-        <div class="iconInfoMatch">&#9731;</div>
-    </div>
 
-    <div class="divInfoUser">
-        <div>400 jugadores han pasado por aquí</div>
-        <div class="iconInfoUser">&#9879;</div>
+    <img v-if="loading" src="../assets/loading.gif" alt="Loading" class="loading" />
+
+    <div v-else>
+        <div class="divInfoMatch">
+            <div class="textInfo">{{ totalMatchs }} partidas jugadas hasta hoy.</div>
+            <div class="iconInfoMatch">&#9731;</div>
+        </div>
+
+        <div class="divInfoUser">
+            <div class="textInfo">400 jugadores han pasado por aquí</div>
+            <div class="iconInfoUser">&#9879;</div>
+        </div>
     </div>
 
 </template>
 
 <script lang="js">
-    export default {}
+    import axios from 'axios';
+
+    export default {
+        created() {
+            this.fetchTotalGame();
+        },
+        data() {
+            return {
+                loading: false
+            };
+        },
+        methods: {
+            fetchTotalGame() {
+                this.loading = true;
+                axios.get('game/GetNumberMatchs')
+                    .then(response => {
+                        this.totalMatchs = response.data;
+                        setTimeout(() => {
+                            this.loading = false;
+                        }, 1000);
+                    }).catch(error => console.log(error))
+            },
+        }
+    }
 </script>
 
 <style scoped>
